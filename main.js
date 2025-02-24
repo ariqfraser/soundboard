973ba0386fc4d3d2b46ed03e7d552502283febc7
ariq <ariqfraser@gmail.com>
2025-02-24 18:31:27 +0000

    adding deps

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 800 });
-  win.loadFile("dist/standalone-v1/browser/index.html");
+    win = new BrowserWindow({ width: 800, height: 800 });
+    win.loadFile("dist/standalone-v1/browser/index.html");
 }
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'standalone-v1';
+    title = 'standalone-v1';
 }
