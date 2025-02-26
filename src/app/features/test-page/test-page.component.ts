7d27725c3f6335afc1aad0d33f80d3daad80a237
ariq <ariqfraser@gmail.com>
2025-02-26 11:59:30 +0000

    tooltips

--- src/app/features/test-page/test-page.component.ts
+++ src/app/features/test-page/test-page.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { IconComponent } from '../../shared/ui/icon/icon.component';
 import { TestPageService } from './test-page.service';
 import { NotificationStatus } from '../../services/notification/notification.types';
+import { TooltipDirective } from '../../shared/directives/tooltip.directive';
 
 @Component({
     selector: 'app-test-page',
-    imports: [IconComponent],
+    imports: [IconComponent, TooltipDirective],
     templateUrl: './test-page.component.html',
     styleUrl: './test-page.component.scss',
 })
---
+++ src/app/shared/directives/tooltip.directive.ts
@@ -0,0 +1,62 @@
+import { Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { Directive, ElementRef, HostListener, input } from '@angular/core';
+import { TooltipComponent } from '../ui/tooltip/tooltip.component';
+
+@Directive({
+    selector: '[appTooltip]',
+})
+export class TooltipDirective {
+    appTooltip = input.required<string>();
+
+    private overlayRef?: OverlayRef;
+
+    constructor(
+        private elemRef: ElementRef,
+        private overlay: Overlay,
+    ) {}
+
+    @HostListener('mouseenter')
+    onEnter() {
+        const positionStrategy = this.overlay
+            .position()
+            .flexibleConnectedTo(this.elemRef.nativeElement)
+            .withPositions([
+                {
+                    originX: 'center',
+                    originY: 'bottom',
+                    overlayX: 'center',
+                    overlayY: 'top',
+                    offsetY: 5,
+                },
+                {
+                    originX: 'center',
+                    originY: 'top',
+                    overlayX: 'center',
+                    overlayY: 'bottom',
+                    offsetY: -5,
+                },
+            ]);
+
+        const overlayConfig: OverlayConfig = {
+            positionStrategy,
+            hasBackdrop: false,
+            scrollStrategy: this.overlay.scrollStrategies.reposition(),
+        };
+        const overlayRef = this.overlay.create(overlayConfig);
+        this.overlayRef = overlayRef;
+
+        const portal = new ComponentPortal(TooltipComponent);
+        const componentRef = overlayRef.attach(portal);
+        const instance = componentRef.instance;
+        instance.text = this.appTooltip();
+    }
+
+    @HostListener('mouseleave')
+    onLeave() {
+        if (this.overlayRef) {
+            this.overlayRef.dispose();
+            this.overlayRef = undefined;
+        }
+    }
+}
